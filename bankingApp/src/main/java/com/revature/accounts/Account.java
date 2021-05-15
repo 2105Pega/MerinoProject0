a39d511ce57279c0efbac74db6fa06c684f40467
Luis Merino <luis.pablo.merino.m@gmail.com>
2021-05-15 17:57:47 -0400

    Added transfer method. Tested transfer

--- bankingApp/src/main/java/com/revature/accounts/Account.java
+++ bankingApp/src/main/java/com/revature/accounts/Account.java
@@ -11,23 +11,35 @@ public class Account implements Serializable {
 	 */
 	private static final long serialVersionUID = -921326158968652254L;
 	private final Integer accountNumber;
+	private final String accountType;
 	private double balance;
 	private String approved;
 	private ArrayList<Customer> customerList;
 	
 	
-	public Account(double balance, Customer ...customers ) {
+	
+	public Account(double balance, String type, Customer ...customers ) {
 		this.balance = balance;
 		Random rand = new Random();
 		Integer generated = rand.nextInt(900000000) + 100000000;
 		accountNumber = generated;
 		approved = "Pending";
+		if (type == "Checking" || type ==  "checking") {
+			this.accountType = "Checking";
+		} else {
+			this.accountType ="Savings";
+		}
 		this.customerList = new ArrayList<Customer>();
 		for (Customer customer: customers) {
 			customerList.add(customer);
 			customer.addAccount(this);
 			
 		}
+		
+		
+	}
+	public String getAccountType() {
+		return this.accountType;
 	}
 	
 	public int getAccountNumber() {
@@ -54,11 +66,11 @@ public class Account implements Serializable {
 	public String toString() {
 		switch (this.getApproved()) {
 		case "Pending":
-			return "account [" + this.getAccountNumber() + "] is pending and has a balance of: " + this.getBalance() + ".";
+			return this.getAccountType() + " account [" + this.getAccountNumber() + "] is pending and has a balance of: " + this.getBalance() + ".";
 		case "Approved":
-			return "account [" + this.getAccountNumber() + "] has a balance of: " + this.getBalance() + ".";
+			return this.getAccountType() + " account [" + this.getAccountNumber() + "] has a balance of: " + this.getBalance() + ".";
 		default:
-			return "account [" + this.getAccountNumber() + "] has been canceled.";
+			return this.getAccountType() + " account [" + this.getAccountNumber() + "] has been canceled.";
 		}
 	}
 	
